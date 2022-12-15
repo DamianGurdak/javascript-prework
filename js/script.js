5af65dbb4844d1abe35e50bd8661be75d973da9f
Damian Gurdak <damiangurdakk@gmail.com>
2022-12-15 01:21:13 +0100

    add player move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+///////////////////Odczytanie ruchu komputera///////////////////
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -13,3 +15,25 @@ if (randomNumber == 1) {
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+///////////////////Odczytanie ruchu gracza///////////////////
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  computerMove = 'papier';
+} else if (playerInput == '3') {
+  computerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+
+///////////////////Wynik gry///////////////////
